0ee6ab473460ee8b9f369154cb54e98ff9b57331
Alejandro Morales <alex94morales@hotmail.com>
2023-01-10 14:17:15 -0600

    1practicajs

--- web/web07/script.js
+++ web/web07/script.js
@@ -1,18 +1,22 @@
-console.log("mensaje desde el scrip externo");
+console.log("Mensaje desde el script externo");
 
 function changeColor(){
-
     document.getElementById("demo").style.color = "red";
 }
 
-function changeColorH2(reference,color){
-
+function changeColorH2(reference, color){
     console.log(reference);
     console.log(color);
-    /* document.getElementById("color").style.color=color; */
-    reference.style.color="green"
+    //document.getElementById("color-h2").style.color = color;
+    reference.style.color = color;
 }
 
-function changeColorByClass (color){
-    const colecction = document.getElementsByClassName
+function changeColorByClass(color){
+    const collection = document.getElementsByClassName("example");
+    console.log(collection); 
+    for (let index = 0; index < collection.length; index++) {        
+        collection[index].style.color = color;
+    }
+
 }
+
