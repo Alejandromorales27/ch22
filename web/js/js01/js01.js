ad23ad53e3b8b268e49f1ebee3abd6629f90155e
Alejandro Morales <alex94morales@hotmail.com>
2023-01-11 13:48:10 -0600

    apuntes y practica

--- web/js/js01/js01.js
+++ web/js/js01/js01.js
@@ -1,1 +1,41 @@
 console.log("js01")
+
+/* son 7 datos primitivos
+-String
+-Number(1800)(los numeros ocupan 64 bits para almacen y solo se usan 53 para representarloss)
+-BigInt
+convertir un dato numerio bigint bigIntvar = bigInt(maxNumber);
+-Boolean
+-undefined 
+-null
+intencionalmente se elimina o se borra el tipo de dato
+-Symbol
+
+tipo de datos object:
+
+-object
+let myobject={}; myObject['clave'] myObject.
+se sugiere poner como const por seguridad y no cambie de dato tipo o se reasignen.
+ejemplo
+domicilios.david;
+domicilios.alberto= "calle siempre viva" esto es para agregar
+domicilios.jessica="calle tulipanes"; remplaza la calle
+delete domicilios.david asi se borra un atributo
+-arrays 
+se pone const por las mismas condiciones que los object
+para leerlo se pone la variable y entre corchote el indice 
+myArray[1] y para modificar se ocupa lo anterior y con igual se remplaza. el metodo join saca un string.
+
+funciones
+
+conversiones de datos:
+
+de string a number
+parseInt este es para enteros Number.parseInt
+parseFloat este es para decimales 
+Number.isNaN(); con el operador + se convierte a numero tambien numberparseInt(1000,2) se puede agregar la base el valor binario se pone la base en la cual queremos nos represente Math.pow una funcion para poner potencia
+
+conversiones de datos de number a string
+(102).toString(radix)
+*/
+
