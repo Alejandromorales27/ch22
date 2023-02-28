76c486485c6925196b624297525417e68aed5a71
Alejandro Morales <alex94morales@hotmail.com>
2023-02-28 11:11:51 -0600

    proyecto repositorio

---
+++ Springboot/ecommercech22-gradle/src/main/java/org/generation/app/Ecommercech22GradleApplication.java
@@ -0,0 +1,13 @@
+package org.generation.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecommercech22GradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecommercech22GradleApplication.class, args);
+	}
+
+}
